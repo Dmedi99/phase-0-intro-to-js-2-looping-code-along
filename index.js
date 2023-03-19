3bb0cd4cc25ad2629fa16bdedfcadf276dcb6f72
David <davidmedina0907@gmail.com>
2023-03-18 20:22:39 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = []
+
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length;i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+    return messages
+}
+
+writeCards(["Nat", "Eric", "Sebastian"], "birthday")
+
+function countDown(){
+    let number = 10
+    while (number >= 0){
+        console.log(number--)
+    }
+}
